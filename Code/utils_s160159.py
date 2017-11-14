e38c8191f04a937d094ce0a9e568a6260ef4568d
Anders Bæk <anders@launer.dk>
2017-11-14 14:52:38 +0100

    ready to train

--- Code/utils_s160159.py
+++ Code/utils_s160159.py
@@ -10,6 +10,27 @@ from skimage import io
 from scipy.misc import imread, imresize
 import matplotlib.pyplot as plt
 
+
+def save_weights(graph , fpath):
+    sess = tf.get_default_session()
+    variables = graph.get_collection("variables")
+    variable_names = [v.name for v in variables]
+    kwargs = dict(zip(variable_names, sess.run(variables)))
+    np.savez(fpath, **kwargs)
+
+
+def load_weights(graph, fpath):
+    sess = tf.get_default_session()
+    variables = graph.get_collection("variables")
+    data = np.load(fpath)
+    for v in variables:
+        if v.name not in data:
+            print("could not load data for variable='%s'" % v.name)
+            continue
+        print("assigning %s" % v.name)
+        sess.run(v.assign(data[v.name]))
+
+
 def safe_div(x,y):
     return np.divide(x,y,where=y != 0)
 
