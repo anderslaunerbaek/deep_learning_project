098f4ce8ca90dca8067c738dc91a9a0e42bb4ca0
Anders Bæk <anders@launer.dk>
2017-11-20 11:43:02 +0100

    sd

--- Code/test.py
+++ Code/test.py
@@ -4,28 +4,28 @@
 
 # In[ ]:
 
-from __future__ import absolute_import, division, print_function 
-import os
-import re
-import sys
-import time
+#from __future__ import absolute_import, division, print_function 
+#import os
+#import re
+#import sys
+#import time
 
-sys.path.append(os.path.join('.', '..')) 
-import utils
-import utils_DL
-import utils_s160159 as u_s
+#sys.path.append(os.path.join('.', '..')) 
+#import utils
+#import utils_DL
+#import utils_s160159 as u_s
 
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import tensorflow as tf
 
-from sklearn.model_selection import LeaveOneOut, KFold
-from sklearn.metrics import confusion_matrix
+#from sklearn.model_selection import LeaveOneOut, KFold
+#from sklearn.metrics import confusion_matrix
 
 
 # In[ ]:
 
-VERSION = '1.1'
-FILENAME = 'master'
+#VERSION = '1.1'
+#FILENAME = 'master'
 
 print(VERSION)
