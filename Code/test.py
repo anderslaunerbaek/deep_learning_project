a636324c144bde0b5cdf974f749bcd5e62d2ee2a
Anders Bæk <anders@launer.dk>
2017-11-20 11:40:54 +0100

    test py.

---
+++ Code/test.py
@@ -0,0 +1,31 @@
+ coding: utf-8
+
+# # Master notebook
+
+# In[ ]:
+
+from __future__ import absolute_import, division, print_function 
+import os
+import re
+import sys
+import time
+
+sys.path.append(os.path.join('.', '..')) 
+import utils
+import utils_DL
+import utils_s160159 as u_s
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from sklearn.model_selection import LeaveOneOut, KFold
+from sklearn.metrics import confusion_matrix
+
+
+# In[ ]:
+
+VERSION = '1.1'
+FILENAME = 'master'
+
+print(VERSION)
